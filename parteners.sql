76b25aed3e2cb0f1c746bed9cb771aea82808910
aza93 <43993728+aza93@users.noreply.github.com>
2018-11-13 10:28:37 +0100

    Add files via upload

---
+++ parteners.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  mar. 13 nov. 2018 à 09:28
+-- Version du serveur :  5.7.23
+-- Version de PHP :  7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `manager`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `parteners`
+--
+
+DROP TABLE IF EXISTS `parteners`;
+CREATE TABLE IF NOT EXISTS `parteners` (
+  `partener_id` int(11) NOT NULL AUTO_INCREMENT,
+  `partener_entreprise` varchar(255) NOT NULL,
+  `partener_activite` varchar(255) NOT NULL,
+  `partener_departement_soucripteur` varchar(255) NOT NULL,
+  `partener_nom` varchar(255) NOT NULL,
+  `partener_tel` varchar(255) NOT NULL,
+  `partener_portable` varchar(255) NOT NULL,
+  `partener_email` varchar(255) NOT NULL,
+  `partener_ca_rapporte` text NOT NULL,
+  `partener_historique` text NOT NULL,
+  `partener_contrat` varchar(255) NOT NULL,
+  `partener_debut_contrat` date NOT NULL,
+  `partener_fin_contrat` date NOT NULL,
+  PRIMARY KEY (`partener_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `parteners`
+--
+
+INSERT INTO `parteners` (`partener_id`, `partener_entreprise`, `partener_activite`, `partener_departement_soucripteur`, `partener_nom`, `partener_tel`, `partener_portable`, `partener_email`, `partener_ca_rapporte`, `partener_historique`, `partener_contrat`, `partener_debut_contrat`, `partener_fin_contrat`) VALUES
+(1, 'Microsoft', 'OS', 'lorem ipsum', 'Bill Gates', '(206) 709-3400', '(206) 709-3400', 'info@gatesfoundation.org', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...', 'Integer laoreet faucibus libero sit amet convallis.', 'Contrat 1', '2017-10-02', '2018-11-08');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
