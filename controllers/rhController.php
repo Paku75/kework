2c2c5a8e42fd33d5d8bcf04fa0fb0ee7fb2e182b
Paku75 <paku2957@gmail.com>
2018-11-14 14:17:59 +0100

    Update controllers/rhController.php

--- controllers/rhController.php
+++ controllers/rhController.php
@@ -8,6 +8,7 @@
     {
         $nom = $_POST['nom'];
         $prenom = $_POST['prenom'];
+        $naissance = $_POST['naissance'];
         $ss= $_POST['ss'];
         $email = $_POST['email'];
         $tel = $_POST['tel'];
@@ -19,6 +20,7 @@
         $access_interface = $_POST['access_interface'];
         
         add_rh($nom,$prenom,$naissance,$ss,$email,$tel,$adresse,$poste,$date_entree,$date_sortie,$departement,$access_interface);
+        var_dump($nom);
     }
     
     
